66b7b477967f2af4ac0f6f8b3ec06afd1ac6bf98
aas86 <aas86@mail.ru>
2019-01-21 10:56:47 +0700

    need to commit

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,4 +1,8 @@
 
+
+import net.aksingh.owmjapis.api.APIException;
+import net.aksingh.owmjapis.core.OWM;
+import net.aksingh.owmjapis.model.CurrentWeather;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 
@@ -17,6 +21,22 @@ public class Bot extends TelegramLongPollingBot {
             SendMessage message = new SendMessage()
                     .setChatId(update.getMessage().getChatId())
                     .setText(update.getMessage().getText());
+            OWM owm = new OWM("4a4b55c74080d5b7b7d2ab3f842e7beb");
+            try {
+                CurrentWeather cwd = owm.currentWeatherByCityName("Novosibirsk");
+                if (cwd.hasRespCode() && cwd.getRespCode() == 200) {
+                    if (cwd.hasCityName()) {
+                        System.out.println("City: " + cwd.getCityName());
+                    }
+                    if (cwd.hasMainData() && cwd.getMainData().hasTempMax() && cwd.getMainData().hasTempMin()) {
+                        System.out.println("Temperature: " + Math.round(cwd.getMainData().getTemp() - 273) + " C");
+
+                    }
+                }
+            } catch (APIException e) {
+                e.printStackTrace();
+            }
+
             try {
                 System.out.println(update.getMessage().getText());
                 execute(message);
